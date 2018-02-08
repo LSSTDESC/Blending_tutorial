9e4d5d64a0ad042bdd55a49539ea2ceb5014e397
fjaviersanchez <francs1@uci.edu>
2018-02-08 08:23:54 -0800

    Fixed bug

--- lsst-kernel2.sh
+++ lsst-kernel2.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 export OMP_NUM_THREADS=4
-setup lsst_sims
 INST_DIR=/global/common/software/lsst/cori-haswell-gcc/stack/
 source $INST_DIR/setup_w_2017_46_py3_gcc6.sh
 setup meas_algorithms
 setup lsst_apps
+#setup lsst_sims # In the case that we need it in the future
 export PYTHONPATH=$PYTHONPATH:/global/projecta/projectdirs/lsst/groups/WL/projects/wl-btf/WeakLensingDeblending/descwl
 exec python -m ipykernel $@
