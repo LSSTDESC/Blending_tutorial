d266a1bf6a122c0e21d49bd14c34780cb544a6e6
fjaviersanchez <francs1@uci.edu>
2018-02-08 10:29:40 -0800

    Removed lsst-kernel2, added sims as optional dependency for process

--- lsst-kernel2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-export OMP_NUM_THREADS=4
-#setup lsst_sims
-INST_DIR=/global/common/software/lsst/cori-haswell-gcc/stack/
-source $INST_DIR/setup_w_2017_46_py3_gcc6.sh
-setup meas_algorithms
-setup lsst_apps
-export PYTHONPATH=$PYTHONPATH:/global/projecta/projectdirs/lsst/groups/WL/projects/wl-btf/WeakLensingDeblending/descwl
-exec python -m ipykernel $@
